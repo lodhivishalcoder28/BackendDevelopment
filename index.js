b84a5c275d989738a3a4afcc5d72c4dec5495e50
vishal Lodhi <lodhivishal286@gmail.com>
2025-06-30 11:33:42 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const app=express()
 require('dotenv').config()
 const port=4000;
 
+const githubdata="vishal is grreat its github id is lodhivishalcoder28";
+
+
 app.get('/',(req,res)=>{
     res.send("hello world");
 })
 
+app.get("/github",(req,res)=>{
+    res.send(githubdata);
+})
 
 app.get('/twitter',(req,res)=>{
     res.send("this is twittter");
